f2800966ad0232ad934b4df81fb1e8a0b67472c2
nordme <nordme@uw.edu>
2019-12-09 09:31:59 -0800

    updates to genz scripts

--- aud_GrandAve.py
+++ aud_GrandAve.py
@@ -11,207 +11,134 @@ script to create auditory grand averages for genZ data.
 # 124 no faces learn 
 
 import mne
+import os
+import itertools
 from os import path as op
 
-age = '17a' ## CHANGE TO APPROPRIATE AGE GROUP [9a, 11a, 13a, 15a, or 17a]
+# set what type of run you want to do
+fixed_or_twa = 'fixed'
+vis_or_aud = 'aud'
+by_all = True
+by_gender = True
 
-# data_path = '/mnt/brainstudio/genz/genz_proc/active/'
-data_path = '/brainstudio/MEG/genz/genz_proc/active/'
+# establish key variables
+ages = ['9a', '11a', '13a', '15a', '17a']
+data_path = '/storage/genz_active/t1/%s_hp/' % fixed_or_twa
 avg_path = op.join(data_path, 'group_averages')
 analysis = 'Split'
 lpf = 80
 
-nine = ['genz105_9a', 'genz106_9a', 'genz108_9a', 'genz111_9a',
-        'genz110_9a', 'genz112_9a', 'genz113_9a', 'genz114_9a', 'genz115_9a',
-        'genz116_9a', 'genz118_9a', 'genz119_9a',
-        'genz120_9a', 'genz121_9a', 'genz122_9a', 'genz123_9a',
-        'genz124_9a', 'genz125_9a', 'genz126_9a', 'genz128_9a',
-        'genz130_9a', 'genz131_9a', 'genz133_9a']
+# prep subject lists
+all_subjects = [x for x in os.listdir(data_path) if 'genz' in x and op.isdir(data_path + x)]
+nine = []
+eleven = []
+thirteen = []
+fifteen = []
+seventeen = []
 
-eleven = [ 'genz205_11a', 'genz206_11a', 'genz207_11a',
-           'genz208_11a', 'genz209_11a', 'genz210_11a', 'genz212_11a',
-           'genz216_11a', 'genz218_11a', 'genz219_11a', 'genz223_11a',
-           'genz224_11a', 'genz225_11a', 'genz226_11a', 'genz227_11a',
-           'genz228_11a', 'genz229_11a', 'genz230_11a', 'genz231_11a',
-           'genz232_11a', 'genz233_11a', 'genz235_11a']
+for s in all_subjects:
+    if '9a' in s:
+        nine.append(s)
+    elif '11a' in s:
+        eleven.append(s)
+    elif '13a' in s:
+        thirteen.append(s)
+    elif '15a' in s:
+        fifteen.append(s)
+    elif '17a' in s:
+        seventeen.append(s)
 
-thirteen = ['genz309_13a', 'genz311_13a', 'genz313_13a', 'genz314_13a',
-            'genz318_13a', 'genz319_13a', 'genz320_13a', 'genz322_13a',
-            'genz323_13a', 'genz324_13a', 'genz325_13a', 'genz326_13a', 'genz327_13a', 'genz328_13a', 'genz329_13a',
-            'genz330_13a', 'genz331_13a', 'genz332_13a', 'genz333_13a', 'genz334_13a',
-            'genz335_13a', 'genz337_13a']
+# prep name and condition lists
 
-fifteen = ['genz401_15a', 'genz403_15a', 'genz406_15a',
-           'genz409_15a', 'genz411_15a', 'genz412_15a', 'genz413_15a',
-           'genz414_15a', 'genz415_15a', 'genz417_15a',
-           'genz418_15a', 'genz419_15a', 'genz420_15a', 'genz421_15a',
-           'genz422_15a', 'genz423_15a', 'genz424_15a', 'genz425_15a',
-           'genz426_15a', 'genz427_15a', 'genz428_15a', 'genz429_15a',
-           'genz430_15a', 'genz431_15a', 'genz432_15a']
+blocks = ['emojis', 'faces', 'thumbs']
+stim = ['aud', 'vis']
+conditions = ['learn', 'test']
+aud_syl = ['s01', 's02', 's03']
+vis_syl = ['s01', 's02', 's03', 's04', 's05', 's06', 's07', 's08', 's09', 's10', 's11', 's12']
 
-seventeen = ['genz507_17a', 'genz512_17a', 'genz513_17a',
-             'genz514_17a', 'genz515_17a', 'genz516_17a', 'genz517_17a',
-             'genz518_17a', 'genz519_17a', 'genz520_17a', 'genz521_17a',
-             'genz522_17a', 'genz523_17a', 'genz526_17a', 'genz527_17a',
-             'genz528_17a', 'genz529_17a', 'genz530_17a', 'genz531_17a', 'genz532_17a']
+aud_conditions = []
+aud_names = []
+vis_conditions = []
+vis_names = []
 
+aud_product = list(itertools.product(blocks, conditions, aud_syl))
+vis_product = list(itertools.product(blocks, conditions, vis_syl))
 
-if age == '9a':
-    subjs = nine
-if age == '11a':
-    subjs = eleven
-if age == '13a':
-    subjs = thirteen
-if age == '15a':
-    subjs = fifteen
-if age == '17a':
-    subjs = seventeen
-      
-conditions = ['aud/faces/learn/s01',
-              'aud/faces/learn/s02',
-              'aud/faces/learn/s03',
-              'aud/thumbs/learn/s01',
-              'aud/thumbs/learn/s02',
-              'aud/thumbs/learn/s03',
-              'aud/emojis/learn/s01',
-              'aud/emojis/learn/s02',
-              'aud/emojis/learn/s03',              
-              'aud/faces/test/s01',
-              'aud/faces/test/s02',
-              'aud/faces/test/s03',
-              'aud/faces/test/s04',
-              'aud/faces/test/s05',
-              'aud/faces/test/s06',
-              'aud/faces/test/s07',
-              'aud/faces/test/s08',
-              'aud/faces/test/s09',
-              'aud/faces/test/s10',
-              'aud/faces/test/s11',
-              'aud/faces/test/s12',              
-              'aud/thumbs/test/s01',
-              'aud/thumbs/test/s02',
-              'aud/thumbs/test/s03',
-              'aud/thumbs/test/s04',
-              'aud/thumbs/test/s05',
-              'aud/thumbs/test/s06',
-              'aud/thumbs/test/s07',
-              'aud/thumbs/test/s08',
-              'aud/thumbs/test/s09',
-              'aud/thumbs/test/s10',
-              'aud/thumbs/test/s11',
-              'aud/thumbs/test/s12',
-              'aud/emojis/test/s01',
-              'aud/emojis/test/s02',
-              'aud/emojis/test/s03',
-              'aud/emojis/test/s04',
-              'aud/emojis/test/s05',
-              'aud/emojis/test/s06',
-              'aud/emojis/test/s07',
-              'aud/emojis/test/s08',
-              'aud/emojis/test/s09',
-              'aud/emojis/test/s10',
-              'aud/emojis/test/s11',
-              'aud/emojis/test/s12']
-names = ['aud_faces_learn_s01',
-         'aud_faces_learn_s02',
-         'aud_faces_learn_s03',
-         'aud_thumbs_learn_s01',
-         'aud_thumbs_learn_s02',
-         'aud_thumbs_learn_s03',
-         'aud_emojis_learn_s01',
-         'aud_emojis_learn_s02',
-         'aud_emojis_learn_s03',  
-         'aud_faces_test_s01',
-         'aud_faces_test_s02',
-         'aud_faces_test_s03',
-         'aud_faces_test_s04',
-         'aud_faces_test_s05',
-         'aud_faces_test_s06',
-         'aud_faces_test_s07',
-         'aud_faces_test_s08',
-         'aud_faces_test_s09',
-         'aud_faces_test_s10',
-         'aud_faces_test_s11',
-         'aud_faces_test_s12',              
-         'aud_thumbs_test_s01',
-         'aud_thumbs_test_s02',
-         'aud_thumbs_test_s03',
-         'aud_thumbs_test_s04',
-         'aud_thumbs_test_s05',
-         'aud_thumbs_test_s06',
-         'aud_thumbs_test_s07',
-         'aud_thumbs_test_s08',
-         'aud_thumbs_test_s09',
-         'aud_thumbs_test_s10',
-         'aud_thumbs_test_s11',
-         'aud_thumbs_test_s12',
-         'aud_emojis_test_s01',
-         'aud_emojis_test_s02',
-         'aud_emojis_test_s03',
-         'aud_emojis_test_s04',
-         'aud_emojis_test_s05',
-         'aud_emojis_test_s06',
-         'aud_emojis_test_s07',
-         'aud_emojis_test_s08',
-         'aud_emojis_test_s09',
-         'aud_emojis_test_s10',
-         'aud_emojis_test_s11',
-         'aud_emojis_test_s12']
+for (b, c, s) in aud_product:
+    ac = 'aud/%s/%s/%s' % (b,c,s)
+    an = 'aud_%s_%s_%s' % (b,c,s)
+    aud_conditions.append(ac)
+    aud_names.append(an)
 
-by_all = True
+for (b, c, s) in vis_product:
+    vc = 'vis/%s/%s/%s' % (b,c,s)
+    vn = 'vis_%s_%s_%s' % (b,c,s)
+    vis_conditions.append(vc)
+    vis_names.append(vn)
 
-by_gender = True
+# begin writing grand averages by age group, separated by gender if so specified
+# iterate by age group, by condition code, 
 
 gaves = []
-
 f_gaves = []
-
 m_gaves = []
 
-if by_gender:
-    for j, (cond, name) in enumerate(zip(conditions, names)):
-        m_evokeds = []
-        f_evokeds = []
-        for subj in subjs:
-            if int(subj[4:7]) % 2 == 0:  # if the subject number is even, the subject is a girl
-                gender = 'f'
-            else:
-                gender = 'm'
-            evoked_file = op.join(data_path, '%s' % subj, 'inverse',
-                                  '%s_%d-sss_eq_%s-ave.fif'
-                                  % (analysis, lpf, subj))
-            evoked = mne.read_evokeds(evoked_file, condition=cond,
-                                      baseline=(None, 0))
-            assert evoked.comment == cond
-            if gender == 'f':
-                f_evokeds.append(evoked)
-            else:
-                m_evokeds.append(evoked)
-        print('f_evokeds: %s' % len(f_evokeds))
-        print('m_evokeds: %s' % len(m_evokeds))
-        f_gaves.append(mne.grand_average(f_evokeds))
-        f_gaves[j].comment = 'f_%s' % cond
-        m_gaves.append(mne.grand_average(m_evokeds))
-        m_gaves[j].comment = 'm_%s' % cond
-    mne.write_evokeds(op.join(avg_path, 'f' + 'AUD_%s_%s_N%d-ave.fif'
-                              % (age, analysis, len(f_evokeds))), f_gaves)
-    mne.write_evokeds(op.join(avg_path, 'm' + 'AUD_%s_%s_N%d-ave.fif'
-                              % (age, analysis, len(m_evokeds))), m_gaves)
+for age in ages:
+    conditions = aud_conditions if vis_or_aud == 'aud' else vis_conditions
+    names = aud_names if vis_or_aud == 'aud' else vis_names
+    if age == '9a':
+        subjs = nine
+    if age == '11a':
+        subjs = eleven
+    if age == '13a':
+        subjs = thirteen
+    if age == '15a':
+        subjs = fifteen
+    if age == '17a':
+        subjs = seventeen
+    if by_gender:
+        for j, (cond, name) in enumerate(zip(conditions, names)):
+            m_evokeds = []
+            f_evokeds = []
+            for subj in subjs:
+                if int(subj[4:7]) % 2 == 0:  # if the subject number is even, the subject is a girl
+                    gender = 'f'
+                else:
+                    gender = 'm'
+                evoked_file = op.join(data_path, '%s' % subj, 'inverse',
+                                      '%s_%d-sss_eq_%s-ave.fif'
+                                      % (analysis, lpf, subj))
+                evoked = mne.read_evokeds(evoked_file, condition=cond,
+                                          baseline=(None, 0))
+                assert evoked.comment == cond
+                if gender == 'f':
+                    f_evokeds.append(evoked)
+                else:
+                    m_evokeds.append(evoked)
+            print('f_evokeds: %s' % len(f_evokeds))
+            print('m_evokeds: %s' % len(m_evokeds))
+            f_gaves.append(mne.grand_average(f_evokeds))
+            f_gaves[j].comment = 'f_%s' % cond
+            m_gaves.append(mne.grand_average(m_evokeds))
+            m_gaves[j].comment = 'm_%s' % cond
+        mne.write_evokeds(op.join(avg_path, 'f' + 'AUD_%s_%s_N%d-ave.fif'
+                                  % (age, analysis, len(f_evokeds))), f_gaves)
+        mne.write_evokeds(op.join(avg_path, 'm' + 'AUD_%s_%s_N%d-ave.fif'
+                                  % (age, analysis, len(m_evokeds))), m_gaves)
 
-
-if by_all:
-    for j, (cond, name) in enumerate(zip(conditions, names)):
-        evokeds = []
-        for subj in subjs:
-            evoked_file = op.join(data_path, '%s' %subj, 'inverse',
-                                  '%s_%d-sss_eq_%s-ave.fif'
-                                  % (analysis, lpf, subj))
-            evoked = mne.read_evokeds(evoked_file, condition=cond,
-                                      baseline=(None,0))
-            assert evoked.comment == cond
-            evokeds.append(evoked)
-        gaves.append(mne.grand_average(evokeds))
-        gaves[j].comment = cond
-        mne.write_evokeds(op.join(avg_path, 'AUD_%s_%s_N%d-ave.fif'
-                                  % (age, analysis, len(subjs))), gaves)
+    if by_all:
+        for j, (cond, name) in enumerate(zip(conditions, names)):
+            evokeds = []
+            for subj in subjs:
+                evoked_file = op.join(data_path, '%s' %subj, 'inverse',
+                                      '%s_%d-sss_eq_%s-ave.fif'
+                                      % (analysis, lpf, subj))
+                evoked = mne.read_evokeds(evoked_file, condition=cond,
+                                          baseline=(None,0))
+                assert evoked.comment == cond
+                evokeds.append(evoked)
+            gaves.append(mne.grand_average(evokeds))
+            gaves[j].comment = cond
+            mne.write_evokeds(op.join(avg_path, 'AUD_%s_%s_N%d-ave.fif'
+                                      % (age, analysis, len(subjs))), gaves)
 
---
+++ genz_mnefun_erm_cov_only.py
@@ -0,0 +1,168 @@
+# -*- coding: utf-8 -*-
+"""
+GenZ pilot analysis script.
+
+"""
+# for genz 232: use 'EOG062' as the ECG channel; use ['ECG063', 'EOG061'] as the EOG channels
+
+import os
+import os.path as op
+import mnefun
+import numpy as np
+from genz_score import (score, aud_in_names, aud_in_numbers)
+
+
+fixed_or_twa = 'twa'
+raw_dir = '/storage/genz_active/t1/%s_hp/' % fixed_or_twa
+
+# raw_dir = '/home/nordme/data/genz/'
+
+if fixed_or_twa == 'twa':
+    trans_to = 'twa'
+else:
+    trans_to = (0.0, 0.0, 0.04)
+
+subjs = [x for x in os.listdir(raw_dir) if op.isdir('%s%s' % (raw_dir, x)) and 'genz' in x]
+# subjs = ['genz105_9a']
+subjs.sort()
+
+params = mnefun.Params(tmin=-0.1, tmax=0.75, t_adjust=0, n_jobs=12,
+                       decim=4, n_jobs_mkl=1, proj_sfreq=200,
+                       n_jobs_fir=12, n_jobs_resample=12,
+                       filter_length='auto', lp_cut=80., bmin=-0.1,
+                       lp_trans='auto', bem_type='inner_skull')
+
+params.subjects = subjs
+params.work_dir = raw_dir
+params.subject_indices = np.arange(len(params.subjects))
+#params.subject_indices = np.setdiff1d(np.arange(len(params.subjects)), [])
+params.dates = [(2014, 10, 14)] * len(params.subjects)
+params.structurals = params.subjects
+params.subject_run_indices = None
+params.subjects_dir = '/storage/anat/subjects/'
+params.score = score
+params.run_names = [
+    '%s_emojis_learn_01',
+    '%s_thumbs_learn_01',
+    '%s_faces_learn_01',
+    '%s_emojis_test_01',
+    '%s_faces_test_01',
+    '%s_thumbs_test_01',
+]
+# params.acq_ssh = 'maggie@minea.ilabs.uw.edu'
+params.acq_dir = ['/brainstudio/MEG/genz']
+params.sws_ssh = 'nordme@kasga.ilabs.uw.edu'  # kasga
+params.sws_dir = '/data07/nordme/genz'
+params.sss_type = 'python'
+params.sss_regularize = 'in'
+params.st_correlation = 0.98
+params.trans_to = trans_to
+params.tsss_dur = 20.
+params.movecomp = 'inter'
+params.coil_t_window = 'auto'
+# Trial rejection
+params.reject = dict()
+params.auto_bad_reject = None
+params.ssp_ecg_reject = None
+params.flat = dict(grad=1e-13, mag=1e-15)
+# Which runs and trials to use
+params.get_projs_from = np.arange(6)
+params.inv_names = []
+params.inv_runs = []
+params.pick_events_cov = None
+params.proj_nums = [[1, 1, 0],  # ECG: grad/mag/eeg
+                    [1, 1, 0],  # EOG
+                    [0, 0, 0]]  # Continuous (from ERM)
+params.on_missing = 'ignore'  # some subjects will not complete the paradigm
+params.in_names = aud_in_names
+params.in_numbers = aud_in_numbers
+params.epochs_prefix = 'All'
+params.cov_method = 'empirical'
+params.compute_rank = True
+params.cov_rank = None
+params.force_erm_cov_rank_full = False
+params.runs_empty = ['%s_erm_01'] # use the empty room covariance
+params.bem_type = '5120'
+# The ones we actually want
+params.analyses = [
+    'All',
+    'Split',
+    ]
+params.out_names = [
+    ['aud', 'vis_onset'],
+    params.in_names,
+    ]
+params.out_numbers = [  # these don't matter as long as they don't overlap...
+    [1] * (3 * 12 + 3 * 3) + [2],
+    100000 * np.arange(1, len(params.in_names) + 1),
+    ]
+# do not trial count match for now
+params.must_match = [[]] * len(params.analyses)
+aud_times = [0.09, 0.25]
+vis_times = [0.17, 0.22]
+#params.report_params.update(  # add a couple of nice diagnostic plots
+#    bem=False,  # Using a surrogate
+#    whitening=[
+#        dict(analysis='All', name='aud',
+#             cov='%s-80-sss-cov.fif'),
+#        dict(analysis='Split', name='aud/emojis/learn/s01',
+#             cov='%s-80-sss-cov.fif'),
+#        dict(analysis='All', name='vis_onset',
+#             cov='%s-80-sss-cov.fif'),
+#    ],
+#    sensor=[
+#        dict(analysis='All', name='aud', times=aud_times),
+#        dict(analysis='Split', name='aud/emojis/learn/s01', times=aud_times),
+#        dict(analysis='SPN', name='vis', times=vis_times),
+#    ],
+#    source=[
+#        dict(analysis='All', name='aud',
+#             inv='%s_aud-80-sss-meg-free-inv.fif', times=aud_times,
+#             views=['lat', 'caudal'], size=(800, 800)),
+#        dict(analysis='Split', name='aud/emojis/learn/s01',
+#             inv='%s_aud-80-sss-meg-free-inv.fif', times=aud_times,
+#             views=['lat', 'caudal'], size=(800, 800)),
+#        dict(analysis='SPN', name='vis',
+#             inv='%s_vis-80-sss-meg-free-inv.fif', times=vis_times,
+#              views=['lat', 'caudal'], size=(800, 800)),
+#    ],
+#    psd=False,  # often slow
+#)
+params.report_params.update(  # add a couple of nice diagnostic plots
+    bem=False,  # Using a surrogate
+    whitening=[
+        dict(analysis='All', name='aud'),
+    ],
+    sensor=[
+        dict(analysis='All', name='aud', times=aud_times),
+    ],
+    sensor_topo=[
+        dict(analysis='All', name='aud'),
+    ],
+#    snr=[
+#        dict(analysis='Split', name='aud_faces_learn_s1'),
+#        dict(analysis='Split', name='aud_emojis_learn_s1'),
+#        dict(analysis='Split', name='aud_thumbs_learn_s1'),
+#    ],
+    source_alignment=True,
+    psd=False,  # often slow
+)
+default = True
+
+mnefun.do_processing(
+    params,
+    fetch_raw=False,  # Fetch raw recording files from acq machine
+    do_score=False,  # do scoring
+    push_raw=False,  # Push raw files and SSS script to SSS workstation
+    do_sss=False, # Run SSS remotely
+    fetch_sss=False,  # Fetch SSSed files
+    do_ch_fix=False,  # Fix channel ordering
+    gen_ssp=False,  # Generate SSP vectors
+    apply_ssp=False,  # Apply SSP vectors and filtering
+    write_epochs=False,  # Write epochs to disk
+    gen_covs=True,  # Generate covariances
+    gen_fwd=True,  # Generate forward solutions (and source space if needed)
+    gen_inv=True,  # Generate inverses
+    gen_report=True,
+    print_status=False,
+)
